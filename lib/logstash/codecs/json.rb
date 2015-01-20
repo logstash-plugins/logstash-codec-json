77e84afb97036733adcd176c72af623ab218eddc
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-01-20 15:37:08 -0500

    Removing milestone option

--- lib/logstash/codecs/json.rb
+++ lib/logstash/codecs/json.rb
@@ -10,7 +10,6 @@ require "logstash/json"
 class LogStash::Codecs::JSON < LogStash::Codecs::Base
   config_name "json"
 
-  milestone 3
 
   # The character encoding used in this codec. Examples include "UTF-8" and
   # "CP1252".
